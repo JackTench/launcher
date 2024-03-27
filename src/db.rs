78c34e760c58b6a9a8e3026db59db200deace0e6
JackTench <79285604+JackTench@users.noreply.github.com>
2024-03-27 19:29:25 +0000

    Create SQLite database.

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,23 @@
+use rusqlite::{Connection, Result};
+
 pub struct Game {
     pub id: i32,
     pub name: String,
     pub platform: String,
     pub launch: String,
     pub times: i32,
+}
+
+pub struct Database {
+    conn: Connection,
+}
+
+impl Database {
+
+    // Create database.
+    pub fn new() -> Result<Self> {
+        let conn = Connection::open("launcher.db")?;
+        Ok(Database { conn })
+    }
+
 }
