c54cd2e1068543fb0058964da4ba37e8dd5656f1
JackTench <79285604+JackTench@users.noreply.github.com>
2024-03-27 21:20:47 +0000

    Fixed typo in schema.

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS games (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    plaform TEXT NOT NULL,
+    platform TEXT NOT NULL,
     launch TEXT NOT NULL,
     times INTEGER NOT NULL
 );
