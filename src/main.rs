8c23c7ecbd9f8f6e53c84158cdec004a2e5430f0
Jack Tench <jack.tench00@gmail.com>
2024-03-28 11:17:37 +0000

    Progress on adding games via GUI.

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,8 @@ fn main() {
 
     let window = AppWindow::new().unwrap();
 
-    // Create game for testing purposes. Just runs neofetch.
-    database.add_game(
-        String::from("Neofetch"),
-        String::from("Linux"),
-        String::from("neofetch"),
-    );
     let games: Vec<db::Game> = database.get().unwrap();
     
-
     // Get list of games to send to Slint to generate buttons.
     let mut game_names: Vec<String> = vec![];
     for game in games {
@@ -30,6 +23,12 @@ fn main() {
     let game_names_ss: Vec<SharedString> = game_names.into_iter().map(Into::into).collect();
     let game_names_rc = ModelRc::new(VecModel::from(game_names_ss));
     window.set_games(game_names_rc);
+
+    // Add game via slint GUI.
+    window.on_add_game(move |name, platform, launch| {
+        database.add_game(String::from(name.clone()), String::from(platform.clone()), String::from(launch.clone()));
+        println!("Added {} {} {}", String::from(name.clone()), String::from(platform.clone()), String::from(launch.clone()))
+    });
 
     window.run().unwrap();
 }
